d5b64db62c5ca7acac1b5fec565ce6de5c5bc592
Jacek Chleborowicz <jacek@cosmose.co>
2017-09-07 00:00:55 +0200

    Javadoc added to VariableReplace.

--- test-common/src/main/java/org/bobba/tools/tests/common/utils/VariableReplace.java
+++ test-common/src/main/java/org/bobba/tools/tests/common/utils/VariableReplace.java
@@ -1,6 +1,7 @@
 package org.bobba.tools.tests.common.utils;
 
 import java.util.Map;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,12 +13,25 @@ public class VariableReplace {
         return replaceVariables(text, variables::get);
     }
 
-    public static String replaceVariables(String text, VariableReplaceCallback variableReplaceCallback) {
+    /**
+     * Replaces all variables in given text.
+     * <p>
+     * Example input stream "You can't ${var1} get what you want" has one variable named "var1".
+     * If variableReplaceFunction returns "always" for variable named "var1", then result would be
+     * "You can't always get what you want".
+     *
+     * @param text                    input text.
+     * @param variableReplaceFunction a function which accepts variable name and returns a text which should replace
+     *                                a variable.
+     * @return input text with variable placeholders replaced with values provided by
+     * variableReplaceFunction.
+     */
+    public static String replaceVariables(String text, Function<String, String> variableReplaceFunction) {
         final Matcher matcher = VARIABLE_PATTERN.matcher(text);
         final StringBuffer result = new StringBuffer();
 
         while (matcher.find()) {
-            final String replacement = variableReplaceCallback.replace(matcher.group(1));
+            final String replacement = variableReplaceFunction.apply(matcher.group(1));
             matcher.appendReplacement(result, replacement);
             result.append("");
         }
@@ -27,7 +41,4 @@ public class VariableReplace {
         return result.toString();
     }
 
-    public interface VariableReplaceCallback {
-        String replace(String variableText);
-    }
 }

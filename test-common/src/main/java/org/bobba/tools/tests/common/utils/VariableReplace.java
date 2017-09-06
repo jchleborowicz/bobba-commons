db91bc84c935fae805c40934e7abfefb0e4d9d2e
Jacek Chleborowicz <jacek@cosmose.co>
2017-09-06 23:48:33 +0200

    Minor pattern correction.

--- test-common/src/main/java/org/bobba/tools/tests/common/utils/VariableReplace.java
+++ test-common/src/main/java/org/bobba/tools/tests/common/utils/VariableReplace.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class VariableReplace {
 
-    private static final Pattern VARIABLE_PATTERN = Pattern.compile("\\$\\{([^\\}]*)\\}");
+    private static final Pattern VARIABLE_PATTERN = Pattern.compile("\\$\\{([^}]*)}");
 
     public static String replaceVariables(String text, Map<String, String> variables) {
         return replaceVariables(text, variables::get);
